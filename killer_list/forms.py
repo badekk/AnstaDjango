a521fcb91a5aa0acfe5728c4b83d658c28fbec6d
s21166-pj <pawel.badysiak@gmail.com>
2020-01-11 19:06:31 +0100

    still learning

--- killer_list/forms.py
+++ killer_list/forms.py
@@ -1,18 +1,34 @@
-from .models import Person
+from .models import Person, Mobile, Email
 from django import forms
 
 
 class PersonForm(forms.ModelForm):
     name = forms.CharField(max_length=20, label="First Name")
     surname = forms.CharField(max_length=20, label="Surname")
-    mobile = forms.CharField(label="mobile number", min_length=9, max_length=9, widget=forms.NumberInput, required=False)
-    email = forms.EmailField(required=False)
 
     class Meta:
         model = Person
         fields = [
             'name',
             'surname',
+        ]
+
+
+class MobileForm(forms.ModelForm):
+    mobile = forms.CharField(label="mobile number", min_length=9, max_length=9, widget=forms.NumberInput, required=False)
+
+    class Meta:
+        model = Mobile
+        fields = [
             'mobile',
+        ]
+
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=False)
+
+    class Meta:
+        mode = Email
+        fields = [
             'email',
         ]
--- killer_list/views.py
+++ killer_list/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
-from .forms import PersonForm
+from .forms import PersonForm, EmailForm, MobileForm
 from .models import Person, Mobile, Email
 # Create your views here.
 
 
 def people_add(request):
     form = PersonForm(request.POST or None)
-    if form.is_valid():
+    email_form = EmailForm(request.POST or None)
+    mobile_form = MobileForm(request.POST or None)
+    if form.is_valid() and email_form.is_valid() and mobile_form.is_valid():
         form.save()
+        email_form.save()
+        mobile_form.save()
         form = PersonForm()
-
+        mobile_form = MobileForm()
+        email_form = EmailForm()
     context = {
-        "form": form
+        "form": form,
+        'mobile': mobile_form,
+        'email': email_form,
         }
 
     return render(request, "killer_list/add_people.html", context)
@@ -19,11 +26,7 @@ def people_add(request):
 
 def people_list_view(request):
     queryset = Person.objects.all()
-    mobiles = Mobile.objects.all()
-    emails = Email.objects.all()
     context = {
         "object_list": queryset,
-        "mobile_numbers": mobiles,
-        "email_adresses": emails,
     }
     return render(request, "killer_list/list_of_people.html", context)
